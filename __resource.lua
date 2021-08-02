44e8cfcbd05de47cf8dc2b123ff6cf393dce536c
Nmm07 <65427983+Nmm07@users.noreply.github.com>
2021-08-02 15:19:24 -0500

    Add files via upload

--- __resource.lua
+++ __resource.lua
@@ -1,3 +1,13 @@
+description 'ESX Money Wash'
+
+version '0.1.0'
+
 client_script "client.lua"
-server_script "server.lua"
-server_script "@mysql-async/lib/MySQL.lua"
+
+server_scripts {
+
+    'server.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'version.lua'
+
+}
